f089f5c3b00f00677b15408920b25da3135f58e2
Guilherme Navevaiko Moreira <guilherme_navevaiko@hotmail.com>
2022-03-22 15:46:54 -0300

    rotas de telas

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,1 +1,19 @@
 import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CadastroONG from "../screens/CadastroONG";
+import CadastroUsuario from "../screens/CadastroUsuario";
+import { LoginUsuario } from "../screens/LoginUsuario";
+import LoginONG from "../screens/LoginONG";
+
+const stackNavigation = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="CadastroONG" component={CadastroONG} />
+      <Stack.Screen name="CadastroUsuario" component={CadastroUsuario} />
+      <Stack.Screen name="LoginUsuario" component={LoginUsuario} />
+      <Stack.Screen name="LoginONG" component={LoginONG} />
+    </Stack.Navigator>
+  );
+};
